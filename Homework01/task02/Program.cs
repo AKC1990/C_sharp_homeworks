fa300aa35407130790593a626374bb40795bb514
Kirill Antropov <chs4t-740@mail.ru>
2022-11-22 07:24:13 +0300

    Added Homework01

---
+++ Homework01/task02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int a = 3;
+int b = 5;
+
+if (a == b)
+{
+    Console.Write("Equal numbers. Please, enter the same numbers");
+}
+else
+    if (a > b)
+    {
+        Console.Write("max = ");
+        Console.Write(a);
+    }
+    else
+    {
+        Console.Write("max = ");
+        Console.Write(b);
+    }
---
+++ Homework01/task04/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a = 2;
+int b = 3;
+int c = 7;
+
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.Write(a);
+    }
+    else
+    {
+        Console.Write(c);
+    }
+}
+else
+{
+    if (b > c)
+    {
+        Console.Write(b);
+    }
+    else
+    {
+        Console.Write(c);
+    }
+}
