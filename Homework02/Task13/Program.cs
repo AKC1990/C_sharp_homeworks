9d1e3b0e710c20885f7a4498e24cd98fa1b32f3a
Kirill Antropov <chs4t-740@mail.ru>
2022-11-22 07:32:23 +0300

    Added Homework02 Task13

---
+++ Homework02/Task13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int number = 164580;
+
+if (number >= 100)
+
+{
+    while (number >= 1000)
+    {
+        number = number / 10;
+    }
+
+    Console.WriteLine(number % 10);
+}
+else
+{
+    Console.WriteLine("There is no the third number");
+}
+
