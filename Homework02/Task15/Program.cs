a6505c94772649616d065096fd155b3d9cdcff87
Kirill Antropov <chs4t-740@mail.ru>
2022-11-22 07:58:04 +0300

    Added Homework02 Task15, fix the scheme in Task 13

---
+++ Homework02/Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+
+int day_number = 5;
+if (1 <= day_number && day_number <=5)
+{
+    Console.WriteLine("Weekdays");
+}
+if (6 <= day_number && day_number <=7)
+{
+    Console.WriteLine("Weekend!!!");
+}
