3188076932d4fc559c2157a47d8f9e303516f6da
Kirill Antropov <chs4t-740@mail.ru>
2022-11-25 07:56:25 +0300

    Added Homework04 Task25

---
+++ Homework04/Task25/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 25: Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Math.Pow использовать нельзя
+
+int a = new Random().Next(1, 10);
+int b = new Random().Next(1, 5);
+int result = 1;
+for (int i = 1; i < b + 1; i++)
+{
+    result *= a;
+}
+Console.WriteLine($"{a} ^ {b} = {result}");
