a88ba28ae019f7820db7358b2b841b68e49b97b0
Kirill Antropov <chs4t-740@mail.ru>
2022-11-30 11:28:47 +0300

    Added Homework07 Task01

---
+++ Homework07/Task01/Program.cs
@@ -0,0 +1,74 @@
+﻿// Доделать ввод многочлена
+// https://github.com/iksergey/multi/blob/lesson7/Program.cs
+// *Сделать вывод только нечетных коэффициентов у треугольника распечатайте хотя бы 120 строк
+
+
+int[,] CreateTriangle(int row)
+{
+  int[,] triangle = new int[row, row];
+  for (int i = 0; i < row; i++)
+  {
+    triangle[i, 0] = 1;
+    triangle[i, i] = 1;
+  }
+
+  for (int i = 2; i < row; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      triangle[i, j] =
+      triangle[i - 1, j - 1] + triangle[i - 1, j];
+    }
+  }
+  return triangle;
+}
+
+void PrintTriangle(int[,] triangle)
+{
+    for (int i = 0; i < triangle.GetLength(0); i++)
+  {
+    for (int j = 0; j < triangle.GetLength(1); j++)
+    {
+      if (triangle[i, j] != 0)
+        Console.Write($"{triangle[i, j],4}");
+    }
+    Console.WriteLine();
+  }
+}
+
+int[] GetKoeff(int[,] tr, int pow)
+{
+  int[] row = new int[pow + 1];
+  for (int i = 0; i <= pow; i++)
+  {
+    row[i] = tr[pow, i];
+  }
+  return row;
+}
+
+string PrintPolynom(int[] array)
+{
+  string[] pows = { "", "", "^2", "^3", "^4", "^5", "^6", "^7", "^8", "^9" };
+  string output = String.Empty;
+  for (int i = 0; i < array.Length; i++)
+  {
+    int t = array[i];
+    if (array[i] == 0) continue;
+    else if (i != 0) { output += " + "; }
+    
+    if (i == 0) { output += $"a{pows[array.Length-1]}"; }
+    if (i == array.Length - 1) { output += $"b{pows[array.Length-1]}"; }
+    else { output += $"{t}a{pows[array.Length - i - 1]}*b{pows[i]}"; }
+
+  }
+  return output;
+}
+
+int[,] tr = CreateTriangle(10);                 // Число в скобках указывает на размер треугольника Паскаля
+int row = 9;                                     // Число указывает на степень возведения уравнения (a + b)
+PrintTriangle(tr);
+int[] koeff = GetKoeff(tr, row);                
+Console.WriteLine(String.Join(' ', koeff));
+Console.WriteLine($"(a + b)^{row} = {PrintPolynom(koeff)}");
+Console.WriteLine();
+
